fe96fd7ea036078954be20e9420a430475438877
Gabriel Vlassov <78628740+Gabriel-Vlas@users.noreply.github.com>
2024-05-09 21:29:22 -0300

    Commit inicial

---
+++ ecommerce-microsservicos-main/produtos-api/target/classes/db/migration/V3__INSERT_INTO_CATEGORIAS.sql
@@ -0,0 +1,5 @@
+INSERT INTO `produtos`.`tb_categorias` (`id`, `nome`) VALUES ('1', 'Eletronicos');
+    
+INSERT INTO `produtos`.`tb_categorias` (`id`, `nome`) VALUES ('2', 'Moveis');
+
+INSERT INTO `produtos`.`tb_categorias` (`id`, `nome`) VALUES ('3', 'Brinquedos');
